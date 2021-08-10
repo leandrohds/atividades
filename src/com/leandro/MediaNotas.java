0c0b391b0ca48b53e2c2e618d539fd5286eeaee4
Leandro Heidemann da Silva <leandrohds_bn@hotmail.com>
2021-08-10 20:46:51 -0300

    Adicionado funcionalidades

--- src/com/leandro/MediaNotas.java
+++ src/com/leandro/MediaNotas.java
@@ -1,32 +1,46 @@
 package com.leandro;
 
 import javax.swing.*;
-import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
-import static java.lang.Math.round;
 
 public class MediaNotas {
 
     public static void main(String[] args) {
-        Integer quantidadeAluno = Integer.parseInt(JOptionPane.showInputDialog("Qual o número de aluos?"));
-
-        Map<String, Double> alunosNotas = new HashMap<>();
+        int quantidadeAluno = Integer.parseInt(JOptionPane.showInputDialog("Qual o número de aluos?"));
+        int quantidadeNotas = Integer.parseInt(JOptionPane.showInputDialog("Qual o número de notas?"));
+        Map<String, List<Double>> alunosNotas = new HashMap<>();
+        List<Double> medias = new ArrayList<>();
 
         for (int i = 0; i < quantidadeAluno; i++) {
             String nome = JOptionPane.showInputDialog("Insira o nome do aluno");
-            Double nota = Double.parseDouble(JOptionPane.showInputDialog("Insira a nota do aluno"));
+            List<Double> notas = new ArrayList<>();
 
-            alunosNotas.put(nome, nota);
+            for (int n = 0; n < quantidadeNotas; n++) {
+                Double nota = Double.parseDouble(JOptionPane.showInputDialog("Insira a nota do aluno"));
+
+                notas.add(nota);
+            }
+
+            alunosNotas.put(nome, notas);
         }
 
-        Double valorTodasNotas = alunosNotas
-                .values()
+        alunosNotas.forEach((nome, notas) -> {
+            Double valorNotas = notas.stream().mapToDouble(Double::doubleValue).sum();
+            Double media = valorNotas / notas.size();
+
+            medias.add(media);
+
+            System.out.println(String.format("Nome: %s - %f", nome, media));
+        });
+
+        double mediaTurma = medias
                 .stream()
                 .mapToDouble(Double::doubleValue)
-                .sum();
+                .sum() / medias.size();
 
-        System.out.println(new DecimalFormat("#.##").format(valorTodasNotas / quantidadeAluno));
+        System.out.println(String.format("Média da Turma: %f", mediaTurma));
     }
 }
